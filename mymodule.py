f96b0802f2ddef2a462ad12aab64330fb9738fce
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-03-11 12:51:58 +0530

    Add custom Python module with basic functions

---
+++ mymodule.py
@@ -0,0 +1,31 @@
+'''
+Program Name: Custom Module with Multiple Functions
+
+Description:
+This module contains four user-defined functions that perform
+basic operations such as addition, subtraction, multiplication,
+and greeting a user.
+
+Functions Included:
+1. add(a, b)       → Returns sum of two numbers
+2. subtract(a, b)  → Returns difference of two numbers
+3. multi(a, b)     → Returns multiplication of two numbers
+4. greet(name)     → Returns greeting message
+
+Author: Hamida Badamdi
+
+'''
+
+# Function 1
+def add(a,b):
+    return a + b
+# Function 2
+def subtract(a,b):
+    return a - b
+# Function 3
+def multi(a,b):
+    return a * b
+
+# Function 4
+def greet(name):
+    return "Hello, " + name
